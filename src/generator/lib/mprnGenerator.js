d4bdb2ac6ae0464fdd5b4376fb8e1831332ea1ad
ajhudson <andyhudson1979@gmail.com>
2021-08-02 22:41:44 +0100

    mprn can only start with 10-73 to avoid using IGT's

--- src/generator/lib/mprnGenerator.js
+++ src/generator/lib/mprnGenerator.js
@@ -1,20 +1,21 @@
 function MprnGenerator() {
-  const MIN = 6;
-  const MAX = 10;
-
   const generateRandomWithinRange = (min, max) =>
     Math.floor(Math.random() * (max - min) + min);
 
   const generateRandom = () => Math.floor(Math.random() * 10);
 
   const generate = () => {
-    const len = generateRandomWithinRange(MIN, MAX);
+    const firstTwoDigits = generateRandomWithinRange(10, 73);
+    const len = generateRandomWithinRange(6, 8);
+
     const digits = new Array(len)
       .fill(0)
       .map(() => generateRandom())
       .join("");
 
-    return digits;
+    const mprn = firstTwoDigits.toString() + digits.toString();
+
+    return mprn;
   };
 
   return {
--- src/generator/lib/mprnGenerator.test.js
+++ src/generator/lib/mprnGenerator.test.js
@@ -11,11 +11,14 @@ describe("tests to generate a valid gas MPRN number", () => {
     generator = null;
   });
 
-  it.only("should be between 6 and 10 characters in length", () => {
+  it.only("should be between 8 and 10 characters in length and first 2 digits should be between 10-73", () => {
     const mprn = generator.generate();
+    const firstTwoDigits = Number.parseInt(mprn.substr(0, 2));
 
     expect(mprn).toBeDefined();
-    expect(mprn.length).toBeGreaterThanOrEqual(6);
+    expect(mprn.length).toBeGreaterThanOrEqual(8);
     expect(mprn.length).toBeLessThanOrEqual(10);
+    expect(firstTwoDigits).toBeGreaterThanOrEqual(10);
+    expect(firstTwoDigits).toBeLessThanOrEqual(73);
   });
 });
